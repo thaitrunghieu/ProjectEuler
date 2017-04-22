08054a873f48c3d2e2a82c1d734531d8e29fdb3a
Hieu Thai <thaitrunghieu@gmail.com>
2017-04-22 11:40:58 -0700

    fixed a bug in math formula.

--- Problems/src/com/euler/problem/p0001/MultipleSum.java
+++ Problems/src/com/euler/problem/p0001/MultipleSum.java
@@ -18,7 +18,7 @@ public class MultipleSum {
     }
 
     private static long sumMultiple(int n, int max) {
-        int k = max-1/n;
+        int k = (max-1)/n;
         return n*k*(k+1)/2;
     }
 }
--- Problems/tst/com/euler/problem/p0001/MultipleSumTest.java
+++ Problems/tst/com/euler/problem/p0001/MultipleSumTest.java
@@ -3,12 +3,10 @@ package com.euler.problem.p0001;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static com.euler.problem.p0001.MultipleSum.sumTwoMultiples;
-
 public class MultipleSumTest {
 
     @Test
     public void testSumTwoMultiples() {
-        Assert.assertEquals(23, sumTwoMultiples(3, 5, 10));
+        Assert.assertEquals(23, MultipleSum.sumTwoMultiples(3, 5, 10));
     }
 }
